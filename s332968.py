0817f8f77a2243e33d00565e9ece5df20dbdfa56
giorgia <s332968@studenti.polito.it>
2024-12-21 16:19:03 +0100

    ???

--- s332968.py
+++ s332968.py
@@ -4,7 +4,8 @@ def f0(x: np.ndarray) -> np.ndarray:
     return x[0] - (-0.188*x[1]) #sub(X0, mul(-0.188, X1))
 
 
-def f1(x: np.ndarray) -> np.ndarray: ...
+def f1(x: np.ndarray) -> np.ndarray: 
+    return (0.867*x[0]) #mul(0.867, X0)
 
 
 def f2(x: np.ndarray) -> np.ndarray: ...
