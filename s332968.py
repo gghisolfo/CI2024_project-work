149fa85972a4c94483288b846cd3968fcdc791a9
giorgia <s332968@studenti.polito.it>
2024-12-21 15:49:17 +0100

    playing

---
+++ s332968.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def f0(x: np.ndarray) -> np.ndarray:
+    return x[0] - (-0.188*x[1]) #sub(X0, mul(-0.188, X1))
+
+
+def f1(x: np.ndarray) -> np.ndarray: ...
+
+
+def f2(x: np.ndarray) -> np.ndarray: ...
+
+
+def f3(x: np.ndarray) -> np.ndarray: ...
+
+
+def f4(x: np.ndarray) -> np.ndarray: ...
+
+
+def f5(x: np.ndarray) -> np.ndarray: ...
+
+
+def f6(x: np.ndarray) -> np.ndarray: ...
+
+
+def f7(x: np.ndarray) -> np.ndarray: ...
+
+
+def f8(x: np.ndarray) -> np.ndarray: ...
+
+
+__all__ = ['f0', 'f1', 'f2']
