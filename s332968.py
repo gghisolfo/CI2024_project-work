d2e8cb51727bf1a853f5930309aab00e274cce21
giorgia <s332968@studenti.polito.it>
2025-06-03 15:42:22 +0200

    updates

--- s332968.py
+++ s332968.py
@@ -1,32 +1,41 @@
 import numpy as np
 
-def f0(x: np.ndarray) -> np.ndarray:
-    return x[0] - (-0.188*x[1]) #sub(X0, mul(-0.188, X1))
+def f1(x: np.ndarray) -> np.ndarray:
+    return np.sin(x[0])
+
+def f2(x: np.ndarray) -> np.ndarray:
+    denominator = x[0] + x[0] + x[1] + x[2]  # Simplified as 2*x0 + x1 + x2
+    if np.any(denominator == 0):  # Avoid division by zero
+        raise ValueError("Denominator cannot be zero.")
+
+    result = np.sign(-x[0] + (52.4 / denominator)) * 4.79e6
+    return result
+
+def f3(x: np.ndarray) -> np.ndarray:
+    result = ((((4.9127 * np.abs(1.7364 * x[0])) - (17.737 * 1.5036 * x[1])) - 2.9209 * 1.1914 *
+              x[2]) + 5.0619 * np.cos(-1.3349 * x[0])) + 51.8489 * np.sin(0.5644 * x[1])
+    return result
 
 
-def f1(x: np.ndarray) -> np.ndarray: 
-    return (0.867*x[0]) #mul(0.867, X0)
+def f4(x: np.ndarray) -> np.ndarray: 
+    result = (x[0] * -0.0909) + (np.cos(x[1]) * 7.00) + 3.28
+    return result
 
 
-def f2(x: np.ndarray) -> np.ndarray: ...
+def f5(x: np.ndarray) -> np.ndarray: 
+    result = (x[0] ** x[1] - 16.2) * (-1.00e-11)
+    return result
+
+def f6(x: np.ndarray) -> np.ndarray: 
+    result = ((1.2832 * 1.3207 * x[1]) - 0.4743 * 1.464 * x[0])
+    return result
 
 
-def f3(x: np.ndarray) -> np.ndarray: ...
+def f7(x: np.ndarray) -> np.ndarray:
+    result = 4.44 ** (x[0] * x[1])
+    return result
 
 
-def f4(x: np.ndarray) -> np.ndarray: ...
-
-
-def f5(x: np.ndarray) -> np.ndarray: ...
-
-
-def f6(x: np.ndarray) -> np.ndarray: ...
-
-
-def f7(x: np.ndarray) -> np.ndarray: ...
-
-
-def f8(x: np.ndarray) -> np.ndarray: ...
-
-
-__all__ = ['f0', 'f1', 'f2']
+def f8(x: np.ndarray) -> np.ndarray: 
+    result = (x[5]**2) * (x[5]**3) * 5.00 - 520
+    return result
